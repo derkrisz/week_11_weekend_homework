274d3ccdf7ab7ff1760630580a44666a3fdd8b98
derkrisz <krisztian.dergmail.com>
2018-01-26 13:27:43 +0000

    add addtask method to populate tasks

--- specs/hero_spec.js
+++ specs/hero_spec.js
@@ -4,13 +4,25 @@ var Hero = require('../hero.js')
 describe('Hero', function(){
 
   var hero;
+  var task1;
+  var task2;
+  var task3;
 
   beforeEach(function() {
     hero = new Hero("Krisz", 100, "Pizza");
   })
 
-  it('should talk', function(){
+  it('should talk', function() {
     assert.strictEqual(hero.talk(), "Hello, my name is Krisz!")
+  })
+
+  it('should have no tasks', function() {
+    assert.strictEqual(hero.tasks.length, 0);
+  })
+
+  it('should be able to add task to tasks', function() {
+    hero.addTask(task1);
+    assert.strictEqual(hero.tasks.length, 1);
   })
 
 })
